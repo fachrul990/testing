dbc0d8ab5ab6ec0734daacc7c0118b076d9996a7
fachrul990 <57552713+fachrul990@users.noreply.github.com>
2020-05-15 18:48:00 +0700

    Add files via upload

---
+++ kel17_for/src/kel17_for/Kel17_for.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kel17_for;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kel17_for {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int i;
+         for (i=0;i<=10;i++){
+         System.out.println("Ini contoh perulangan for ke-"+i);
+                      }  
+       
+    }
+    
+}
+TODO code application logic here
+    }
+    
+}
